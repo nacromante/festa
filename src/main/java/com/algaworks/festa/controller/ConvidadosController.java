c6d75b09bc63c8e09750ed97ffc31c304422d3bd
fred <fredericond@hotmail.com>
2018-01-08 01:17:19 -0300

    primeiro

--- src/main/java/com/algaworks/festa/controller/ConvidadosController.java
+++ src/main/java/com/algaworks/festa/controller/ConvidadosController.java
@@ -1,38 +1,83 @@
 package com.algaworks.festa.controller;
 
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.algaworks.festa.model.Convidado;
-import com.algaworks.festa.repository.Convidados;
+import com.algaworks.festa.model.Sexo;
+import com.algaworks.festa.repository.ConvidadosRep;
 
 @Controller
-@RequestMapping("/convidados")
+//@RequestMapping("/convidados")
 public class ConvidadosController {
 	
 	@Autowired
-	private Convidados convidados;
+	private ConvidadosRep convidados;
 	
-	@GetMapping
-	public ModelAndView listar() {
-		
+	public ConvidadosController() {
+	}
+	
+	@GetMapping("/convidados")
+	public ModelAndView listar(Convidado convidado) {
 		ModelAndView modelAndView = new ModelAndView("ListaConvidados");
 		modelAndView.addObject("convidados", convidados.findAll());
-		modelAndView.addObject(new Convidado());
+		modelAndView.addObject(convidado);
 	
 		return modelAndView;
 	}
 	
-	@PostMapping
-	public String salvar(Convidado convidado) {
+	@PostMapping("/convidado/salvar")
+	public ModelAndView salvar(@Valid Convidado convidado, BindingResult result, RedirectAttributes attr) {
+		System.out.println("conv id ="+ convidado.getId());
+		if(result.hasErrors())
+			return cadastro(convidado);
+		convidado.setDataPresencaConfirmada(new Date());
+		attr.addFlashAttribute("msg", "Convidado salvo com sucesso!");
+		if(convidado.getId() != null)
+			attr.addFlashAttribute("convidado", convidado);
 		convidados.save(convidado);
-		return "redirect:/convidados";
+		return new ModelAndView("redirect:/cadastro");
 
 	}
+	@GetMapping("/add/{id}")
+	public String add(@PathVariable("id") Long id, RedirectAttributes attr) {
+		Convidado convidado = convidados.findOne(id);
+		if(convidado != null)
+			attr.addFlashAttribute("convidado", convidado);
+		return "redirect:/cadastro";
+	} 
+	
+	@GetMapping("/excluir/{id}")
+	public String excluir(@PathVariable Long id) {
+		convidados.delete(id);
+		return "redirect:/convidados";
+	}
+	
+	@GetMapping("/cadastro")
+	public ModelAndView cadastro(Convidado convidado) {
+		ModelAndView modelAndView = new ModelAndView("Convidado");
+		List<Sexo> sexos = Arrays.asList(Sexo.values());
+		modelAndView.addObject("convidado", convidado);
+		modelAndView.addObject("sexos", sexos);
+		return modelAndView;
+	}
+	
+	
 	
 	
 
--- src/main/java/com/algaworks/festa/model/Convidado.java
+++ src/main/java/com/algaworks/festa/model/Convidado.java
@@ -1,13 +1,29 @@
 package com.algaworks.festa.model;
 
 import java.io.Serializable;
+import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotBlank;
+import org.hibernate.validator.constraints.br.CPF;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.format.annotation.NumberFormat;
+import org.springframework.format.annotation.NumberFormat.Style;
 
 @Entity
 public class Convidado implements Serializable {
@@ -20,11 +36,42 @@ public class Convidado implements Serializable {
 	@Column(name="id")
 	private Long id;
 	
-	@Column(name="nome")
+	@Column(nullable=false,name="nome", length=60)
+	@NotBlank(message="Nome é obrigatório")
+	@Size(max=60, message="Nome pode conter no máximo 60 caracteres")
 	private String nome;
 	
+	@Column(name="cpf", length=14)
+	@CPF(message="CPF inválido")
+	@NotBlank(message="CPF é obrigatório")
+	private String cpf;
+	
+	@NotNull(message="Sexo é obrigatório")
+	@Enumerated(EnumType.STRING)
+	private Sexo sexo;
+	
 	@Column(name="quantidade_acompanhantes")
+	@NotNull(message="Quantidade de acompanhantes é obrigatória")
 	private Integer quantidadeAcompanhantes;
+	
+	@DateTimeFormat(pattern="dd/MM/yyyy")
+	@Temporal(TemporalType.DATE)
+	private Date dataNascimento;
+	
+	@DateTimeFormat(pattern="yyyy-MM-dd")
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date dataPresencaConfirmada;
+	
+	@Email(regexp="(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])",
+			message="E-mail inválido")
+	@NotBlank(message="E-mail é obrigatório")
+	private String email;
+	
+	@ManyToOne
+	private Presente presente;
+	
+	@OneToOne
+	private Usuario usuario;
 
 	public Long getId() {
 		return id;
@@ -48,6 +95,62 @@ public class Convidado implements Serializable {
 
 	public void setQuantidadeAcompanhantes(Integer quantidadeAcompanhantes) {
 		this.quantidadeAcompanhantes = quantidadeAcompanhantes;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public Date getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(Date dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+	public Date getDataPresencaConfirmada() {
+		return dataPresencaConfirmada;
+	}
+
+	public void setDataPresencaConfirmada(Date dataPresencaConfirmada) {
+		this.dataPresencaConfirmada = dataPresencaConfirmada;
+	}
+
+	public Presente getPresente() {
+		return presente;
+	}
+
+	public void setPresente(Presente presente) {
+		this.presente = presente;
+	}
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+
+	public Sexo getSexo() {
+		return sexo;
+	}
+
+	public void setSexo(Sexo sexo) {
+		this.sexo = sexo;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
 	}
 	
 	
